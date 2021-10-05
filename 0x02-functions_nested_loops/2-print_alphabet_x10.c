664e183a3f9c3c4a796f66af0e6f2f53b691045c
threadsv1 <majdimed3@gmail.com>
2021-10-05 14:45:13 +0100

    edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,17 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
- *    * Return: Always 0.
- *     */
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
-	char i, j;
-	for (j = 48; j < 58; j++)
+char i, j;
+for (j = 48; j < 58; j++)
 {
-		for (i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
-			_putchar(i);
+_putchar(i);
 }
-		_putchar('\n');
+_putchar('\n');
 }
 }
