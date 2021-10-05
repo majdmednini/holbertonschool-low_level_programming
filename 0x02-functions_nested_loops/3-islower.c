087ebdfce2962a1546c242f7f04e8c4cd3a3a983
threadsv1 <majdimed3@gmail.com>
2021-10-05 15:02:01 +0100

    edited

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * print_alphabet - prints prints the alphabet, in lowercase
+ *   *
+ *    * Return: Always 0.
+ *     */
+void int _islower(int c)
+{
+char str = 'a';
+if(islower(str)==0) 
+{
+	printf("0");
+}
+
+	else 
+{
+	printf("1"):
+}
+}
