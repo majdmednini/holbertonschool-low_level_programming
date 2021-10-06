c84a5cb397dbde27b4cdbb2b59e09b45a99d8638
threadsv1 <majdimed3@gmail.com>
2021-10-06 11:25:29 +0100

    number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int print_sign - checks for alphabetic character
+ * @n: Number to pass to _isalpha function
+ *
+ * Return: 1 (Success)
+ */
+void int print_sign(int n)
+{
+	if (n > 0)
+		return (+);
+	else if (n = 0)
+		return (0);
+	else 
+		return (-);
+}
