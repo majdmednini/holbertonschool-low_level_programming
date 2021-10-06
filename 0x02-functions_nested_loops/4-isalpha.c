744d065faf0b6ab83f7bfb72ececc4b92db9895b
threadsv1 <majdimed3@gmail.com>
2021-10-06 10:37:31 +0100

    edited

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _isalpha - Test the _isalpha function
+ * @n: Number to pass to _isalpha function
+ * 
+ * Return: 1 (Success)
+ */
 int _isalpha(int c);
 {
 if (c < 'z' && c > 'a')
