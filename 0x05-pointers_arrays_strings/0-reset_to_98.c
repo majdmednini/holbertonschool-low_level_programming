29089e696fcfb9ae53e3758db01b5a2f1eef28f0
threadsv1 <majdimed3@gmail.com>
2021-10-11 13:28:28 +0100

    edited

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
- *
+ * reset_to_98 - check the code
+ *@n: The character to check
+ *@p: The character to check
  * Return: Always 0.
  */
 void reset_to_98(int *n)
