abf1990479a2605fe4520691ca02e985b6dabdab
threadsv1 <majdimed3@gmail.com>
2021-10-11 14:44:40 +0100

    edited

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void swap_int - check the code
+ *@a: The character to check
+ *@b: The character to check
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 98;
+*b = 42;
+{
+printf("a=%d, b=%d\n", a, b);
+}
+{
+swap_int(&a, &b);
+}
+{
+printf("a=%d, b=%d\n", a, b);
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define main_h
 
 void print_to_98(int n);
+void swap_int(int *a, int *b);
 
 #endif
