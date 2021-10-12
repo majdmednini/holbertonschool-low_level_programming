9f051fc75054c46dc7459747e032a6f8ba197610
threadsv1 <majdimed3@gmail.com>
2021-10-12 11:24:57 +0100

    edited

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - check the code
+ *@*str  The character to check
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+char *str[100] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+{
+_puts(str);
+}
+return (0);
+}
