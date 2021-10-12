1e921c8dbcaabd60e0cd4e291f70af4e2c961641
threadsv1 <majdimed3@gmail.com>
2021-10-12 07:36:40 +0100

    edited

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _strlen - check the code
+ *@s: The character to check
+ *@: The character to check
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+printf("%d", strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void print_to_98(int n);
 void swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 
 #endif
