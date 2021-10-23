99fac857fcf120aa17789ce531f71f7e36492bba
threadsv1 <majdimed3@gmail.com>
2021-10-23 16:27:03 +0100

    edited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 0;
+while (a < 27)
+{
+	putchar("%c \n", a);
+	a++;
+}
+return 0;
+}
