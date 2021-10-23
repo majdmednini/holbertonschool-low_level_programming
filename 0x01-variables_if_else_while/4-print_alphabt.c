181d6b27fba1c121575272e0d10aae657937b29b
threadsv1 <majdimed3@gmail.com>
2021-10-23 18:04:38 +0100

    edited

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch,chh,c;
+for (ch = 'a' ; ch <= 'd' ; ch++)
+{
+putchar(ch);
+}
+for (c = 'f' ; c <= 'p' ; c++)
+{
+putchar(ch);
+}
+for (chh = 'r' ; chh <= 'z' ; chh++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
