c59c57d0ac581d7b2c0183d354ce1c570e1eb95c
threadsv1 <majdimed3@gmail.com>
2021-10-24 22:05:31 +0100

    edited

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+for (ch = 'a' ; ch <= 'f'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
