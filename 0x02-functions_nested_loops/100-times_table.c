70e4fd1e64d068d56b2f22e60facc4db607d76b3
threadsv1 <majdimed3@gmail.com>
2021-10-27 09:48:41 +0100

    edited

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @n: integer
+ * Return: returns nothing
+ */
+int main(void)
+{
+int main(void)
+{
+int i,n,a,b,sum;
+while (i < 1024)
+{
+n = 3 * i;
+i++;
+printf("%d", n);
+}
+while (a < 1024)
+{
+b = 5 * a;
+a++;
+printf("%d", b);
+}
+printf("%d + %i", n, b);
+printf("\n");
+return (0);
+}
