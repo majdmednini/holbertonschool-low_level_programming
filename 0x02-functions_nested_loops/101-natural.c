b454400afde0813279983c76c428a82fe55f6c5f
threadsv1 <majdimed3@gmail.com>
2021-10-27 09:56:11 +0100

    edited

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,13 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * @n: integer
- * @i: integer
- * @a: integer
- * @b: integer
- * @sum: integer
- * Return: Always 0.
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-int main(void)
-{
-int i,n,a,b,sum;
-while (i < 1024)
-{
-n = 3 * i;
-i++;
-printf("%d", n);
-}
-while (a < 1024)
-{
-b = 5 * a;
-a++;
-printf("%d", b);
-}
-printf("%d + %i", n, b);
-printf("\n");
-return (0);
+
+		return (0);
 }
