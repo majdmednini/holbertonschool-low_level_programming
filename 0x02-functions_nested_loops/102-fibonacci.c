cc558c1cfcb4c9fe402d7a8d00d5753eaa7f5569
threadsv1 <majdimed3@gmail.com>
2021-10-27 10:31:55 +0100

    edited

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+while (i < 50)
+{
+i++;
+}
+printf("\n");
+return (0);
+}
