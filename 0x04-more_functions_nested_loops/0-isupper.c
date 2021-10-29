eb60b7e958c3b98269ad966bfb05f51b107f64aa
threadsv1 <majdimed3@gmail.com>
2021-10-29 10:51:49 +0100

    edited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 /**
  * _isupper - checks for uppercase character
  *@c: The character to check
- *
  * Return: 1 (success), 0 (otherwize)
  */
 int _isupper(int c)
 {
 if (c >= 65) && (c <= 90))
 {
-	return (1);
+return (1);
 }
 else
 {
-	return (0);
+return (0);
 }
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,5 +13,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
+int _isupper(int c);
 
 #endif
