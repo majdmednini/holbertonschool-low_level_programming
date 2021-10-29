f9e40b66e591abe504aa02897f0a82066d4cd1bc
threadsv1 <majdimed3@gmail.com>
2021-10-29 11:24:44 +0100

    edited

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - multiplies two integers
+ *@i: The character to check
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+int i = 0;
+while (i < 10)
+{
+_putchar("%d", i);
+}
+_putchar("\n");
+return (0);
+}
