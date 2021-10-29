cd72bf8b5750fc8c245a4202d8b824241c36f55c
threadsv1 <majdimed3@gmail.com>
2021-10-29 10:58:15 +0100

    edited

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,5 +14,14 @@ void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
 
 #endif
