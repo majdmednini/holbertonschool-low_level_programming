97742887c572c44822297ad48fd0e26875824f80
threadsv1 <majdimed3@gmail.com>
2021-10-29 11:03:30 +0100

    edited

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _isdigit - checks for a digit
- * @c: The character to check
- *
- * Return: 1 (success), 0 (otherwize)
+ * int mul - multiplies two integers
+ * @a: The character to check
+ * @b: The character to check
+ * Return: Always 0 
  */
 int mul(int a, int b);
 {
