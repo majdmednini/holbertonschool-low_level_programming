8dfbc6263af5a535d129021e9b60d9282dd675b9
threadsv1 <majdimed3@gmail.com>
2021-10-29 11:11:08 +0100

    edited

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * int mul - multiplies two integers
- * @a: The character to check
- * @b: The character to check
- * Return: Always 0 
+ * mul - multiplies two integers
+ *@a: The character to check
+ *@b: The character to check
+ * Return: Always 0.
  */
 int mul(int a, int b);
 {
