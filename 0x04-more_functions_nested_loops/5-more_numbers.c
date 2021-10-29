3d7b1c441ffceb0ac30483328e6e5a7a187f442f
threadsv1 <majdimed3@gmail.com>
2021-10-29 13:35:02 +0100

    edited

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ for (i = 0; i <= 10; i++)
 {
 for (n = 0; n <= 14; n++)
 {
-if (n > 9)
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+if (i > 9)
+_putchar((i/ 10) + '0');
+_putchar((i % 10) + '0');
 }
 }
 _putchar('\n');
