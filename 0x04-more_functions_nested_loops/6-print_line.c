5035f214d82e2095d44ccbe9c99d26b4af12bf23
threadsv1 <majdimed3@gmail.com>
2021-11-03 09:30:50 +0100

    edited

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  *print_line - draws a straight line
+ *   * Return: Always 0.
+ *    */
+void print_line(int n)
+{
+	int main()
+	{
+		char str[100];
+		int i, totChar, n;
+		totChar = 0;
+		for (i=0; str[i] != '\0'; i++)
+		{
+			if (str[i]!=' ')
+			{
+				totChar++;
+			}
+			if (i != 0)
+			{
+				for ( n = 0; n < i; n++)
+				{
+					printf("_")
+				}
+			}
+		}
+	}
+}
