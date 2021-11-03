dfa0873f2ee1c0e0fbf768f819b1e5bcf75fe331
threadsv1 <majdimed3@gmail.com>
2021-11-03 11:04:48 +0100

    edited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,22 @@
-#include "main.h"
 #include <stdio.h>
 /**
+ * main - Prints the numbers from 1-100 with FizzBuzz game
  *
- *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i;
-for(i = 1; i <= 100; ++i)
+int i, n;
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-printf("Fizz");
-if (i % 5 == 0)
-printf("Buzz");
-if ((i % 3 != 0) && (i % 5 != 0))
-printf("number=%d", i);
-printf("\n");
+if (i % 15 == 0)
+printf("FizzBuzz\t");
+else if ((i % 3) == 0)
+printf("Fizz\t");
+else if ((i % 5) == 0)
+printf("Buzz\t");
+else
+printf("%d\t", i);
 }
 return (0);
 }
