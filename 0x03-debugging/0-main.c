23685e91f04e1c12f8d235451d65daca2e59eba2
threadsv1 <majdimed3@gmail.com>
2021-11-03 12:21:17 +0100

    edited

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,15 +6,10 @@
  * positive_or_negative - posit or nega
  * Return: Always 0.
  */
-void positive_or_negative(int i)
+int main(void)
 {
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
-	printf("%d is negative\n", n);
-else if (n > 0)
-	printf("%d is positive\n", n);
-else
-	printf("%d is zero\n", n);
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
