89a9f1f5e6438b78e3fd8701faa3a243a9aacb44
threadsv1 <majdimed3@gmail.com>
2021-11-06 11:46:27 +0100

    edited

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_rev - check the code
+ * rev_string - check the code
  *@s: char
  *
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 int c, len = _strlen(s);
 for (c = len - 1; c >= 0; c--)
