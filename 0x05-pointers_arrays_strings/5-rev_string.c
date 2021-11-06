4f508ef543ca0e025c953ef643b9190279d1ca9d
threadsv1 <majdimed3@gmail.com>
2021-11-06 11:57:37 +0100

    edited

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,14 @@
  */
 void rev_string(char *s)
 {
-rev_string(s);
+int len = 0, i = 0;
+char x;
+while (s[i++])
+len++;
+for (i = len - 1; i >= len / 2; i--)
+{
+x = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = x;
+}
 }
