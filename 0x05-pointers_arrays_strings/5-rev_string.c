b773ab9203949e8d9843e3c4885bd776b9826725
threadsv1 <majdimed3@gmail.com>
2021-11-06 11:52:08 +0100

    edited

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,5 @@
  */
 void rev_string(char *s)
 {
-int c, len = _strlen(s);
-for (c = len - 1; c >= 0; c--)
-_putchar(s[c]);
-_putchar('\n');
-}
-/**
- *_strlen - returns the length of a string
- *@s: char
- * Return: len (Success)
- */
-int _strlen(char *s)
-{
-int len = 0;
-while (*s++)
-len++;
-return (len);
+rev_string(s);
 }
