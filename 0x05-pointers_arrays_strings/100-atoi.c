c80d1948fa9af0f25269130da14f55a1c6256c5b
threadsv1 <majdimed3@gmail.com>
2021-11-10 11:19:02 +0100

    edited

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - prints every other character of a string
+ *@s: integer
+ * Return: The integer value of the converted string
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+unsigned int number = 0;
+do
+{
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+number = (number * 10) + (*s - '0');
+else if (number > 0)
+break;
+} while (*s++);
+return (number * sign);
+}
