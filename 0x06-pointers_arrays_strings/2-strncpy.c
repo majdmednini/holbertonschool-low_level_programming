34e937b3b0acfd609ca5ec7f5fa07c6dcaf22e2a
threadsv1 <majdimed3@gmail.com>
2021-11-10 23:07:49 +0100

    edited

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncpy - prints every other character of a string
+ *@dest: char
+ *@src: char
+ *@n: integer
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, 98);
+for (int i = 0; i < ; i++)
+{
+printf("%c", dest[i]);
+}
+return (0);
+}
