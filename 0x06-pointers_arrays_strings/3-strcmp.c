6254af2e322b828f6552218755d355535cf68e98
threadsv1 <majdimed3@gmail.com>
2021-11-10 23:45:46 +0100

    edited

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - concatenates two strings
+ *@s1: char
+ *@s2: char
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+i = 0;
+while (i < n && s2[i] != '\0')
+{
+s1[i] == s2[i];
+i++;
+}
+i = i;
+while (i < n)
+{
+s1[i] = '\0';
+i++;
+}
+return (0);
+}
