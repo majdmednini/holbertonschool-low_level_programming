485fec9dff6b8d1bb81b9a561a8375ea0ef0f132
threadsv1 <majdimed3@gmail.com>
2021-11-10 23:52:15 +0100

    edited

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,20 @@
 #include "main.h"
 /**
- * _strcmp - concatenates two strings
+ * _strcmp - compares two strings
  *@s1: char
  *@s2: char
- * Return: 0
+ * Return: integer
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, n;
-i = 0;
-while (i < n && s2[i] != '\0')
 {
-s1[i] == s2[i];
+int i = 0;
+int j = 0;
+while (s1[i] != '\0' && j == 0)
+{
+j = s1[i] - s2[i];
 i++;
 }
-i = i;
-while (i < n)
-{
-s1[i] = '\0';
-i++;
+return (j);
 }
-return (0);
 }
