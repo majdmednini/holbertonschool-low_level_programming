1418e9d496c72f2dc37f29eb15f2cc33ac86517a
threadsv1 <majdimed3@gmail.com>
2021-11-11 11:06:16 +0100

    fixed

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - compares two strings
+ *@a: int
+ *@n: int
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+i = i;
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (0);
+}
