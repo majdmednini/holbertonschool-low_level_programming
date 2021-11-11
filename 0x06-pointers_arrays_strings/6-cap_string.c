c26aa3584dfb0412883bda1482ce3576cad4a014
threadsv1 <majdimed3@gmail.com>
2021-11-11 12:35:43 +0100

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,25 @@
  */
 char *cap_string(char *str)
 {
-int i;
+int i, j = 0;
+char ch[] = {',', '\t', '\n', ' ', ';', '.','!', '?', '"', '(', ')', '{', '}'};
 i = 0;
 while (str[i] != '\0')
 {
-if (str[i] >= 97 && str[i] <= 122)
+if (str[i] >= 'a' && str[i] <= 'z')
 {
+if (i == 0)
+{
+str[0] = str[0] - 32;
+}
+else
+{
+for (j < 13; j++)
+{
+if (str[i - 1] == ch[j])
 str[i] = str[i] - 32;
 }
-if (str[i] >= 0 && str[i] <= 96)
-{
-str[i] = str[i];
+}
 }
 i++;
 }
