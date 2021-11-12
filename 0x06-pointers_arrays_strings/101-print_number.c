ada36b9acc05cd9e7c648ce674ad0ee3170a0f7e
threadsv1 <majdimed3@gmail.com>
2021-11-12 11:48:46 +0100

    edited

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ *@n: integer
+ * Return: int
+ */
+void _number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+else if (n == 0)
+_putchar('0');
+else ((n / 10) > 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
