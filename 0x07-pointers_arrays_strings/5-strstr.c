536cad15bbf9714d3db82fe56b609cf1efe3d112
threadsv1 <majdimed3@gmail.com>
2021-11-18 19:40:47 +0100

    edited

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ *@s: char
+ *@accept: char
+ * Return: a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
