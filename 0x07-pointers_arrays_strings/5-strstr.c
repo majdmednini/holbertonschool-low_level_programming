21cb5d7ca1c6ab26ba12fa8fdaef2697b4ac5f42
threadsv1 <majdimed3@gmail.com>
2021-11-18 19:44:50 +0100

    edited

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,29 @@
 #include "main.h"
+int _strlen(char *s);
 /**
- * *_strpbrk - searches a string for any of a set of bytes
- *@s: char
- *@accept: char
- * Return: a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ * _strstr - function that locates a substring
+ *@haystack: pointer on a string
+ *@needle: pointer on a substring
+ *
+ * Return: a pointer or 0
  */
 char *_strstr(char *haystack, char *needle)
 {
-
+int i;
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+i = 0;
+if (haystack[i] == needle[i])
+{
+do {
+if (needle[i + 1] == '\0')
+return (haystack);
+i++;
+} while (haystack[i] == needle[i]);
+}
+haystack++;
+}
+return ('\0');
+}
