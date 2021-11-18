19d2234fc31f9352b3cbf74609a5b4cc7df6d6b7
threadsv1 <majdimed3@gmail.com>
2021-11-19 00:45:04 +0100

    edited

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-int len = 0, i = 0;
-char x;
-while (s[i++])
-len++;
-for (i = len - 1; i >= len / 2; i--)
+if (*s)
 {
-x = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = x;
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
