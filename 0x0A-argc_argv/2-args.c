12e10b8f6939fc27f7186046f7f9bcfca345662b
threadsv1 <majdimed3@gmail.com>
2021-11-20 14:48:04 +0100

    edited

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,8 +6,8 @@
  * @argv: char
  * Return: Always 0.
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-printf("%d\n", (argc - 1));
+printf("%s\n", argv[0]);
 return (0);
 }
