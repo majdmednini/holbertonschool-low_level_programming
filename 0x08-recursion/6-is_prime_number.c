e8f0832be971b5849d7da76b867ccf1d4aa0c0e8
threadsv1 <majdimed3@gmail.com>
2021-11-20 09:09:20 +0100

    edited

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+ *@n: int
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+for (int i = 2; i < n; i++)
+{
+if (n % i == 0)
+{
+return (1);
+}
+else 
+return (0);
+}
+}
