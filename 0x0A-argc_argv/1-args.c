b55340ab5b3afb731d9a17631df1af7c648ca4e1
threadsv1 <majdimed3@gmail.com>
2021-11-20 14:36:57 +0100

    edited

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -  prints the number of arguments passed into it
+ * @argc: int
+ * @argv: char
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", (argc - 1));
+return (0);
+}
