0a5c0cb6a5c2448d03b5bd2ec5f3cd17b059b097
threadsv1 <majdimed3@gmail.com>
2021-11-22 17:40:27 +0100

    edited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,34 @@
 #include "main.h"
-#include <stlib.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- *@n: int
+ *@filename: char
+ *@letters: size_t
  * Return: Always 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buf = malloc(sizeof(char) * letters);
-ssize_t rl, rw;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDWR);
-if (fd == -1)
-return (0);
-rl = read(fd, buf, letters);
-if (rl < 0)
-return (0);
-rw = write(STDOUT_FILENO, buf, rl);
-if (rw < 0)
-return (0);
-free(buf);
-close(fd);
-return (rw);
+	char *mybuf = malloc(sizeof(char) * letters);
+	int fd;
+	ssize_t rl, rw;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+	rl = read(fd, mybuf, letters);
+	if (rl < 0)
+		return (0);
+	rw = write(STDOUT_FILENO, mybuf, rl);
+	if (rw < 0)
+		return (0);
+	free(mybuf);
+	close(fd);
+	return (rw);
 }
