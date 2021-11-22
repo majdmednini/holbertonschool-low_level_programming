84e109a3f5e5b25ad56a08c47bc6365c35ef4c6a
threadsv1 <majdimed3@gmail.com>
2021-11-22 16:14:29 +0100

    edited

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *@n: int
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
