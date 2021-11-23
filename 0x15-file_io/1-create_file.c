28544c740b08ff0ea501ebe9b4c97b8d5812064a
threadsv1 <majdimed3@gmail.com>
2021-11-23 10:57:03 +0100

    edited

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 int fd, rw, lenght = 0;
 if (filename == 0)
 return (-1);
-fd = open(filename, O_RDWR);
+fd = open(filename, O_RDWR | O_WRONLY | O_CREAT | O_TRUNC );
 if (fd < 0)
 return (-1);
 if (text_content != NULL)
