ef3a6130373fe1189c73abbb8facc1002dbbf801
threadsv1 <majdimed3@gmail.com>
2021-11-23 21:40:38 +0100

    edited

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[lenght] != '\0'; lenght++)
+for (lenght= 0; text_content[lenght] != '\0'; lenght++)
 continue;
 wr = write(fd, text_content, lenght);
 if (wr == -1)
