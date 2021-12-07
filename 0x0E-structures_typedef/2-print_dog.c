95966dfed7e813cfc2c9e38a43dca5ae2230a93f
threadsv1 <majdimed3@gmail.com>
2021-12-07 14:08:48 +0100

    edited

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ return;
 
 
 if (d->name == NULL)
-	printf("Name: %p\n", d->name);
+	printf("Name: (nil)\n");
 else
 {
 	printf("Name: %s\n", d->name);
 	printf("Age: %.6f\n", d->age);
 }
 if (d->owner == NULL)
-	printf("Owner: %p\n", d->owner);
+	printf("Owner: (nil)\n");
 else
 {
 	printf("Owner: %s\n", d->owner);
