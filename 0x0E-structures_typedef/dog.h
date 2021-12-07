8c2448118ce417113738ade38cc68c73e4dfe3a2
threadsv1 <majdimed3@gmail.com>
2021-12-07 13:13:51 +0100

    edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
 /**
  * struct dog - Short description
  * @name: char
@@ -7,15 +8,22 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 char name;
 float age;
 char owner;
-};
-int main()
-{
-struct dog my_dog = {Poppy, 3.5, Bob};
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
 }
+dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+
+
+
+
+#endif
