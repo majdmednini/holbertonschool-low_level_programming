e2638b9ccbcd3ab13ca202134a8e3dc3418a8c4f
threadsv1 <majdimed3@gmail.com>
2021-12-07 12:59:47 +0100

    edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,14 @@
-
+/**
+ * struct dog - Short description
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char name;
+float age;
+char owner;
+};
