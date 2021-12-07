0d5c617160e3ec6d7e933064c8f815571c85aec1
threadsv1 <majdimed3@gmail.com>
2021-12-07 13:09:25 +0100

    edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * struct dog - Short description
  * @name: char
@@ -12,3 +13,9 @@ char name;
 float age;
 char owner;
 };
+int main()
+{
+struct dog my_dog = {Poppy, 3.5, Bob};
+printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+return (0);
+}
