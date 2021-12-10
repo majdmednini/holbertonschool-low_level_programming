e2d21f063b05c6666e29163854b53127b007afae
threadsv1 <majdimed3@gmail.com>
2021-12-10 13:08:42 +0100

    edited

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[])
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0
+ */
+int main(void)
 {
-printf("%s\n",argv[0]);
+printf("%s\n", __FILE__);
 return (0);
 }
