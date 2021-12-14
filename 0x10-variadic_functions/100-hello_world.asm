471fefa874eb5b0ff4fc7a9a01e2e043cfa59687
threadsv1 <majdimed3@gmail.com>
2021-12-14 01:42:35 +0100

    edited

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,10 +1,10 @@
-section .data
-message db "Hello, World", 10
-section .text
-global main
+	section .data
+	msg db "Hello, World",10
+	section .text
+	global main
 main:
-mov rax, 1
-mov rdi, 1
-mov rsi, message
-mov rdx, 17
-syscall
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 13
+	syscall
