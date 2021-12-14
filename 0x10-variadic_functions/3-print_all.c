6efe4089fa37e68a45b25152ffcd5366f2f1209d
threadsv1 <majdimed3@gmail.com>
2021-12-14 01:30:10 +0100

    edited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,21 +8,21 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	char c, *ch, *separator = "";
-	va_list ap;
-	va_start(ap, format);
-	while (format && format[i])
-	{
-		c = format[i];
-		switch (c)
-		{
-			case 'c':
-				printf("%s%c", separator, va_arg(ap, int));
-				separator = ", ";
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(ap, int));
+unsigned int i = 0;
+char c, *ch, *separator = "";
+va_list ap;
+va_start(ap, format);
+while (format && format[i])
+{
+c = format[i];
+switch (c)
+{
+case 'c':
+printf("%s%c", separator, va_arg(ap, int));
+separator = ", ";
+break;
+case 'i':
+printf("%s%d", separator, va_arg(ap, int));
 separator = ", ";
 break;
 case 'f':
