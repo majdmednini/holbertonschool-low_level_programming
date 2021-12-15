d07c491bf276dac7b9fb99e94434cef0bcc9e81b
threadsv1 <majdimed3@gmail.com>
2021-12-15 14:07:33 +0100

    edited

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Finds and prints the largest prime
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long n, i;
+n = 612852475143;
+for (i = 2; i < n; i++)
+{
+while (n % i == 0)
+n = n / i;
+}
+printf("%lu\n", n);
+return (0);
+}
