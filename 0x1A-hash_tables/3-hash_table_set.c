b8c3302e6a9b532a9a636de6e8af8f2a55d32586
threadsv1 <majdimed3@gmail.com>
2022-01-28 18:01:12 +0100

    edited

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to hash table
+ * @ht: hash table to update
+ * @key: key
+ * @value: value of key
+ * Return: updated table
+ */
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+
+/**
+ * char *hash_table_get - function that retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: key
+ * Return: value associated with the element
+ */
