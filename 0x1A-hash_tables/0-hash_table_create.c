79fba14fec0aff2b8c1a921b77d4ef8ec740d420
threadsv1 <majdimed3@gmail.com>
2022-01-28 17:00:58 +0100

    edited

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,4 +10,10 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
+	hash_table_t *hash_t;
+	hash_t =malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+		return
+hash_t->size = size;
+return (hash_t);
+}
